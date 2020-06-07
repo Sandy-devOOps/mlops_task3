996da1ee8e2ea2c1737654cee6646c0a6da553f0
anikash322 <55351573+anikash322@users.noreply.github.com>
2020-06-07 04:17:35 -0700

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -87,18 +87,8 @@ model.save("mnist_LeNet.h5")
 # Evaluate the performance of our trained model
 scores = model.evaluate(x_test, y_test, verbose=1)
 accuracy_score=scores[1]
-f=open("output.txt","w")
+f=open("/root/mlops/output.txt","w")
 f.write(str(100*accuracy_score))
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
 
-accuracy_file = open('/root/mlops/accuracy.txt','w')
-accuracy_file.write(str(scores[1]))
-accuracy_file.close()
-
-display_matter = open('/root/mlops/display_matter.html','r+')
-display_matter.read()
-display_matter.write('<pre>\n---------------------------------------------\n')
-display_matter.write(this_layer)
-display_matter.write('\nAccuracy achieved : ' + str(scores[1])+'\n</pre>')
-display_matter.close()
